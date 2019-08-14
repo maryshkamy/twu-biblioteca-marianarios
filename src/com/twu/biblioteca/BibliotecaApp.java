857aa779f126bed907d29a6f400423c31b635e16
Mariana Rios Silveira Carvalho <marianariossc@gmail.com>
2019-08-14 13:40:56 -0300

    1.2 - Add a list of all books after the welcome message.

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -4,5 +4,19 @@ public class BibliotecaApp {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
+
+        System.out.println("\nBook's list:");
+        String[] books = new String[] {
+                "Harry Potter and the Sorcerer's Stone",
+                "Harry Potter and the Chamber of Secrets",
+                "Harry Potter and the Prisoner of Azkaban",
+                "Harry Potter and the Goblet of Fire",
+                "Harry Potter and the Order of the Phoenix",
+                "Harry Potter and the Half Blood Prince",
+                "Harry Potter and the Deathly Hallows"};
+
+        for (int i = 0; i < books.length - 1; i++) {
+            System.out.println(books[i]);
+        }
     }
 }
